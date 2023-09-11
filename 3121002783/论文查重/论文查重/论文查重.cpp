80fb4f17aa69727be39e5be60db1c33ef57bd96b
blockche <blockche@qq.com>
2023-09-12 00:46:48 +0800

    ReadTXT()函数完成
    ReadTXT()，用来读入文件，并返回一个string

--- 3121002783/论文查重/论文查重/论文查重.cpp
+++ 3121002783/论文查重/论文查重/论文查重.cpp
@@ -1,6 +1,32 @@
 ﻿#include "common.h"
 
-int main() {
+using namespace std;
+
+string ReadTXT(char* addr) {
+	ifstream infile;
+	infile.open(addr);
+	//assert(infile.is_open());
+	if (!infile.is_open()) {
+		cout << "文件打开失败。" << endl;
+		exit(0);
+	}
+
+	string s, t;
+	while (infile >> t) {
+		s += t;
+	}
+
+	infile.close();
+	return s;
+}
+
+int main(int argc, char* argv[]) {
+
+	string orig = ReadTXT(argv[1]);
+	string orig_add = ReadTXT(argv[2]);
+
+	cout << orig << endl;
+	cout << orig_add << endl;
 
 	return 0;
 }
